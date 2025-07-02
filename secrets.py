3723df26311a9b1fe4fc8cbe6a73d6c2ef18000c
JeanIvna <micheldjamel08@gmail.com>
2025-07-02 13:09:53 +0000

    Create secrets.py
    Add secrets.py with API tokens

---
+++ secrets.py
@@ -0,0 +1,5 @@
+# secrets.py
+
+GITHUB_USERNAME = "JeanIvna"  # Remplace par ton nom GitHub exact
+GITHUB_TOKEN = "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"
+RENDER_API_KEY = "rnd_BzpJaSZW1EPgE56dDdHyX0fiVOID"
